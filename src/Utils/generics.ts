8710e91af2852f8944fd160bb65a6dc3e7d5ecdd
Sanka Vollereii <arigowok05@gmail.com>
2025-05-18 02:33:42 +0700

    update

--- src/Utils/generics.ts
+++ src/Utils/generics.ts
@@ -206,7 +206,7 @@ export const generateMessageIDV2 = (userId?: string): string => {
 	random.copy(data, 28)
 
 	const hash = createHash('sha256').update(data).digest()
-	return '3EB0' + hash.toString('hex').toUpperCase().substring(0, 18)
+	return 'SANKA-' + hash.toString('hex').toUpperCase().substring(0, 18)
 }
 
 // generate a random ID to attach to a message
